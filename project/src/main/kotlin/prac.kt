172e2be546cfc7118d443f83ab78ae949d98c2a0
SeYoun Lee <lsy20140@naver.com>
2022-06-28 18:54:58 +0900

    Ch2_part6

--- project/src/main/kotlin/prac.kt
+++ project/src/main/kotlin/prac.kt
@@ -1,29 +1,44 @@
+
+
 fun main() {
-    var count : Int = 85
-
-    // if-else
-    if(count >= 90){
-        println("if문: A학점")
-    }
-    else if(count >= 80){
-        println("if문: B학점")
-    }
-    else if(count >= 70){
-        println("if문: C학점")
-    }
-    else if(count >= 60){
-        println("if문: D학점")
-    }
-    else{
-        println("if문: F학점")
+    // hw02-04
+    for(i in 5..8 ){
+        for(j in 1..9)
+            println("$i X $j = ${i*j}")
+        println("-----------")
     }
 
-    // when -> if-else 보다 가독성 good
-    when(count){
-        in 90 .. 100 -> println("when문: A학점")
-        in 80 .. 89 -> println("when문: B학점")
-        in 70 .. 79 -> println("when문: C학점")
-        in 60 .. 69 -> println("when문: D학점")
-        else -> println("when문: F학점")
+    // hw02-05
+    var sum = 0
+    var x = 0
+
+    while(x <= 100){
+        if(x%2==1){
+            sum+=x
+        }
+        x++
     }
+    println("$sum")
+
+
+    sum = 0
+    x = 0
+
+    for(x in 1..100){
+        if (x%2==1)
+            sum+=x
+    }
+    println("$sum")
+
+    
+    sum = 0
+    x = 0
+
+    for(x in 1..100 step 2){
+        sum += x
+    }
+    println("$sum")
 }
+
+
+
